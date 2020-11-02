a59e94867ed1b4ca8ea8c6ff3a3696c30e484abb
Koki Makishima <MakishimaKouki@kouki.local>
2020-11-03 00:24:26 +0900

    直接法

---
+++ DirectMethod.R
@@ -0,0 +1,18 @@
+#???ږ@
+
+candidate <- 1/pi
+M <- pi/2
+n <- 100000
+
+theta <- runif(n, min = 0, max = pi)
+u <- runif(n, min = 0, max = 1)
+
+condition <- (u < sin(theta))
+
+hist <- (theta[condition])
+
+hist(hist, breaks=seq(0, pi, by=pi/20), 
+main="Histgram of data", xlab="theta", ylab="density", freq=FALSE)
+curve(sin(x)/2, 0, pi, add=TRUE) 
+abline(h=1/2, col="red") 
+abline(h=1/pi, col="blue") 
